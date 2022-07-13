ab66407db49e31932d36254ae8a8c090b6f83def
HacksFor <107859673+HacksFor@users.noreply.github.com>
2022-07-13 05:45:19 -0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -63,15 +63,15 @@ int main()
 {
     //so it basically rewrite game assembly and create internal function hook (you can look at game memory with cheatEngine etc)
     // 
-    //mov[rdx + rcx * 8], 2FAF080 	//new car cost in hex   0x2DF802B-address
+    //mov[rdx + rcx * 8], 2FAF080 	//new car cost in hex
     //jmp gta5.exe + 15DC17E
     //
     //5 bytes we don't care about
     //
-    //mov rax, [GTA5.exe + 2686820]
+    //mov rax, [GTA5.exe + 2686820] //code starts here
     //cmp[rdx + rcx * 8], rax
-    //je gta5.exe + 2DF802B  		//first line
-    //mov rax, [rdx + rcx * 8]
+    //je gta5.exe + 2DF802B  		//jmp to first line
+    //mov rax, [rdx + rcx * 8]      //return rax to initial state, so game won't bug/crash
     //jmp gta5.exe + 15DC17E
     //
     //jmp gta5.exe + 2DF803C        //0x15DCA73 end of function that writes to car value and other addreses
@@ -85,10 +85,9 @@ int main()
 
     const long long int var6 = 5190269934155187433;
 
-    std::cout << "Input your car cost: ";
-    std::cin >> var4;
-
     std::cout << "\nStarted script...\n";
+
+    ReadProcessMemory(processHandle, (void*)(dllBaseAddress + 0x2686820), &var4, sizeof(int), 0);//get cost of vehicle
 
     WriteProcessMemory(processHandle, (void*)(dllBaseAddress + 0x2DF802B), &var1, sizeof(long long int), 0);
     WriteProcessMemory(processHandle, (void*)(dllBaseAddress + 0x2DF8033), &var2, sizeof(long long int), 0);
@@ -100,6 +99,8 @@ int main()
 
     //WriteProcessMemory(processHandle, (void*)(dllBaseAddress + 0x905A52), &line1, sizeof(long long int), 0);//movss xmm6<-[gta5.exe + 6B7F8]
     //WriteProcessMemory(processHandle, (void*)(dllBaseAddress + 0x905A5A), &line2, sizeof(long int), 0);//movss [rsi]<-xmm6
+    //that's part with invincibility hack. Have some issues for now.
+
     std::cout << "Memory Patched...\n";
     std::cout << "If script didn't work (or game crashed) look for updated version github.com/hacksfor/gta5online\n";
     Sleep(10000);//you better to use sleep_for() i just don't care
